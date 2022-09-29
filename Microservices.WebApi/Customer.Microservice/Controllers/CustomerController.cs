20ceafe21d4ce0259a8b58958c16992a3e541442
Ganjali717 <ganjali.imanov@gmail.com>
2022-09-29 11:35:04 +0400

    Add methods for customer controller

---
+++ Microservices.WebApi/Customer.Microservice/Controllers/CustomerController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using Customer.Entity.AppDbContext;
+
+namespace Customer.Microservice.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly CustomerDbContext _context;
+        public CustomerController(CustomerDbContext context) => _context = context;
+
+        [HttpGet]
+        public IActionResult GetAllCustomers()
+        {
+            var issues = _context.Customers.ToList();
+            return issues == null ? NotFound() : Ok(issues);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Entity.Customer), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetbyId(Guid id)
+        {
+            var issue = _context.Customers.FirstOrDefault(x => x.Id == id);
+            return issue == null ? NotFound() : Ok(issue);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public IActionResult AddIssue(Entity.Customer issue)
+        {
+            if (issue == null) return BadRequest("404");
+            _context.Customers.Add(issue);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetbyId), new { id = issue.Id }, issue);
+        }
+
+        [HttpGet("{title}")]
+        [ProducesResponseType(typeof(Entity.Customer), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetbyName(string title)
+        {
+            var issue = _context.Customers.FirstOrDefault(x => x.Name == title);
+            return issue == null ? NotFound() : Ok(issue);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult UpdateIssue(Entity.Customer model)
+        {
+            var issue = _context.Customers.FirstOrDefault(x => x.Id == model.Id);
+            if (issue == null) return NotFound();
+            issue.Name = model.Name;
+            issue.Surname = model.Surname;
+            issue.Email = model.Email;
+            issue.MobilePhone = model.MobilePhone;
+            issue.Address = model.Address;
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
