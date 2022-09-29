7430b4ac9eb3c7bd7f301f6fe182b02b39126cc0
Ganjali717 <ganjali.imanov@gmail.com>
2022-09-29 10:59:32 +0400

    Create Entitity folder, because we need divide it. Each assembly have it's own DatabaseContext

---
+++ Microservices.WebApi/Customer.Entity/Customer.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+
+namespace Customer.Entity
+{
+    public class Customer
+    {
+    }
+}
---
+++ Microservices.WebApi/Product.Entity/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Product.Entity
+{
+    public class Class1
+    {
+
+    }
+}
